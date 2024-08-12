4ea9e8b2a3877e4466013a5fe754d6247e38b5a3
heeba-khan <heebak45@gmail.com>
2024-08-12 20:14:49 +0530

    Added .env

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,13 @@ const cors=require('cors')
 const mysql=require('./config/db')
 require('dotenv').config()
 const bannerRoutes = require('./routes/bannerRoutes')
+const frontendurl=process.env.FRONTEND_URL
 
 const app=express()
 const PORT=process.env.PORT||5000;
 app.use(express.json());
 app.use(cors({
-  origin: 'https://banner-7uj2lzdjc-heebas-projects.vercel.app/', 
+  origin: frontendurl, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true, 
 }));
