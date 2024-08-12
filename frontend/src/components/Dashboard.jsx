879a6c0432e9c69d8182545db79090c5484c57d2
heeba-khan <heebak45@gmail.com>
2024-08-12 23:22:51 +0530

    made final changes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -30,7 +30,7 @@ const Dashboard = ({ setCurrentBanner }) => {
   const toggleVisibility = async () => {
     if (banner) {
       const updatedBanner = { ...banner, visibility: !banner.visibility };
-      await axios.put(https://banner-dashboard-backend.vercel.app/api/banners/${banner.id}, updatedBanner);
+      await axios.put(`https://banner-dashboard-backend.vercel.app/api/banners/${banner.id}`, updatedBanner);
       setBanner(updatedBanner);
       setCurrentBanner(updatedBanner); 
     }
