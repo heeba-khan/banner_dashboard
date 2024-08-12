ebcfce7a6049bd1b1c926d90da97239a9ed21552
heeba-khan <heebak45@gmail.com>
2024-08-12 21:44:52 +0530

    changed few things

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,12 @@ const cors=require('cors')
 const mysql=require('./config/db')
 require('dotenv').config()
 const bannerRoutes = require('./routes/bannerRoutes')
-const frontendurl=process.env.FRONTEND_URL
+
 
 const app=express()
 const PORT=process.env.PORT||5000;
 app.use(express.json());
-app.use(cors({
-  origin: frontendurl, 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  credentials: true, 
-}));
+app.use(cors());
 
 app.use('/api/banners',bannerRoutes)
 
