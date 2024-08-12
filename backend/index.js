9712c6e989e26befbce6f4d29508e9c7b78f98f5
heeba-khan <heebak45@gmail.com>
2024-08-12 22:06:22 +0530

    added home route

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ const PORT=process.env.PORT||5000;
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+
 app.use('/api/banners',bannerRoutes)
 
 //listening to port
